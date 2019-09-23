2c72c566a1904a6e4ffa504edfd3726de685ff9d
Evan Vander Hoeven <vhevan.927@gmail.com>
2019-09-23 12:22:37 -0500

    switch to modern API approach
    I discovered that the previous commit was using out of date code, this is more modern

--- app/src/main/java/com/example/p2pchat/MainActivity.java
+++ app/src/main/java/com/example/p2pchat/MainActivity.java
@@ -10,7 +10,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     // Class variables
-    private final IntentFilter intentFilter = new IntentFilter();
+    private IntentFilter intentFilter = new IntentFilter();
     private WifiP2pManager.Channel channel;
     private WifiP2pManager manager;
     private WiFiDirectBroadcastReceiver receiver;
@@ -20,21 +20,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Setting up the intent filter
-        // Indicates a change in the Wi-Fi P2P status.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        this.manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        this.channel = manager.initialize(this, getMainLooper(), null);
+        this.receiver = new WiFiDirectBroadcastReceiver(this.manager, this.channel, this);
 
-        // Indicates a change in the list of available peers.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        this.intentFilter = new IntentFilter();
+        this.intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        this.intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        this.intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        this.intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
-        // Indicates the state of Wi-Fi P2P connectivity has changed.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        /*
+            Discovering peers, I'm not yet sure that this code belongs in onCreate, because
+            the lifecycle method onCreate is only called once, so if I want a new connection,
+            I'm not sure what I should so. The documentation for discoverPeers (https://developer.android.com/reference/android/net/wifi/p2p/WifiP2pManager.html#discoverPeers(android.net.wifi.p2p.WifiP2pManager.Channel,%20android.net.wifi.p2p.WifiP2pManager.ActionListener)
+            says that the discovery remains active until a connection is initiated or a p2p group is formed.
+            So, maybe at that point it isn't necessary to discover peers anyways.
+         */
+        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                // Only notifies that the discovery process completed.
+                // does not provide information about the discovered peers.
+                // a WIFI_P2P_PEERS_CHANGED_ACTION intent is broadcasted if
+                // we were successful.
+            }
 
-        // Indicates this device's details have changed.
-        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+            @Override
+            public void onFailure(int reasonCode) {
 
-        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        channel = manager.initialize(this, getMainLooper(), null);
+            }
+        });
     } // end of onCreate
 
     /** register the BroadcastReceiver with the intent values to be matched */
@@ -48,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onPause() {
         super.onPause();
-        unregisterReceiver(receiver);
+        unregisterReceiver(receiver); // implementing the LifecycleObserver interface should simplify this code
     }
 
 } // end of class
--- app/src/main/java/com/example/p2pchat/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/com/example/p2pchat/WiFiDirectBroadcastReceiver.java
@@ -4,54 +4,60 @@ import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 
+/**
+ * A BroadcastReceiver that notifies an activity of important Wi-Fi p2p events.
+ */
 public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
 
-    private WifiP2pManager.Channel channel;
     private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
     private Activity activity;
 
-    WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, Activity activity)
-    {
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
+                                       Activity activity) {
         super();
         this.manager = manager;
         this.channel = channel;
         this.activity = activity;
-        // Do additional setup?
     }
 
     @Override
-    public void onReceive(Context context, Intent intent)
-    {
+    public void onReceive(Context context, Intent intent) {
+        // Variable declatation
         String action = intent.getAction();
+        WifiP2pManager.PeerListListener myPeerListListener = new WifiP2pManager.PeerListListener() {
+            @Override
+            public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
+                // take that device list and give it to the activity to display
+                // so that the user can choose what device they want to connect to
+            }
+        };
+
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
-            // Determine if Wifi P2P mode is enabled or not, alert
-            // the Activity.
+            // Check to see if Wi-Fi is enabled and notify appropriate activity
             int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
             if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-                activity.setIsWifiP2pEnabled(true);
+                // Wifi P2P is enabled
             } else {
-                activity.setIsWifiP2pEnabled(false);
+                // Wi-Fi P2P is not enabled
             }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
-
-            // The peer list has changed! We should probably do something about
-            // that.
-
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+            // request available peers from the wifi p2p manager. This is an
+            // asynchronous call and the calling activity is notified with a
+            // callback on PeerListListener.onPeersAvailable()
+            if (manager != null) {
+                manager.requestPeers(channel, myPeerListListener);
+            }
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
-
-            // Connection state changed! We should probably do something about
-            // that.
-
+            // Respond to new connection or disconnections
+            // Applications can use requestConnectionInfo(), requestNetworkInfo(), or requestGroupInfo() to retrieve the current connection information.
         } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
-            DeviceListFragment fragment = (DeviceListFragment) activity.getFragmentManager()
-                    .findFragmentById(R.id.frag_list);
-            fragment.updateThisDevice((WifiP2pDevice) intent.getParcelableExtra(
-                    WifiP2pManager.EXTRA_WIFI_P2P_DEVICE));
-
+            // Respond to this device's wifi state changing
+            // Applications can use requestDeviceInfo() to retrieve the current connection information.
         }
     }
-
 }
