db7bc4a81706960a3a766c9a72e93e9c5404bcf3
Nicholas LaBelle <nrlabelle@gmail.com>
2019-10-16 11:56:33 -0500

    fix binding

--- app/src/main/java/edu/uwstout/p2pchat/ChatFragment.java
+++ app/src/main/java/edu/uwstout/p2pchat/ChatFragment.java
@@ -1,4 +1,4 @@
-package com.example.p2pchat;
+package edu.uwstout.p2pchat;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,7 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.databinding.BindingAdapter;
 import androidx.fragment.app.Fragment;
 
-import com.example.p2pchat.databinding.FragmentChatBinding;
+import edu.uwstout.p2pchat.databinding.FragmentChatBinding;
 
 public class ChatFragment extends Fragment
 {
@@ -26,8 +26,9 @@ public class ChatFragment extends Fragment
     }
 
     @BindingAdapter("android:OnClick")
-    public static void sendButtonClickListener(View view, View.OnClickListener send)
+    public static void setButtonClickListener(View view, View.OnClickListener send)
     {
+        
 
     }
 
