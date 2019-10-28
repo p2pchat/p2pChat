644f71037e612b96961e52bade6969de8a174314
Austin Scott <12504656+Austin-Scott@users.noreply.github.com>
2019-10-27 23:16:56 -0500

    reformat code

--- app/src/main/java/edu/uwstout/p2pchat/ExternalFile.java
+++ app/src/main/java/edu/uwstout/p2pchat/ExternalFile.java
@@ -4,30 +4,44 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 
-public class ExternalFile {
+public class ExternalFile
+{
     File path;
     String mimeType;
-    public ExternalFile(File path, String mimeType) {
+
+    public ExternalFile(File path, String mimeType)
+    {
         this.path = path;
         this.mimeType = mimeType;
     }
-    public InMemoryFile loadIntoMemory() {
+
+    public InMemoryFile loadIntoMemory()
+    {
         byte[] data = new byte[(int) path.length()];
-        try {
+        try
+        {
             DataInputStream dis = new DataInputStream(new FileInputStream(path));
             dis.readFully(data);
             return new InMemoryFile(path.getName(), data, mimeType);
-        } catch(Exception exc) {
+        }
+        catch (Exception exc)
+        {
             return null;
         }
     }
-    public boolean delete() {
+
+    public boolean delete()
+    {
         return path.delete();
     }
-    public boolean exists() {
+
+    public boolean exists()
+    {
         return path.exists();
     }
-    public String getPath() {
+
+    public String getPath()
+    {
         return path.getPath();
     }
 }
--- app/src/main/java/edu/uwstout/p2pchat/InMemoryFile.java
+++ app/src/main/java/edu/uwstout/p2pchat/InMemoryFile.java
@@ -18,55 +18,80 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
-public class InMemoryFile {
+public class InMemoryFile
+{
     byte[] data;
     String filename;
     String mimeType;
 
-    public InMemoryFile(String filename, byte[] data, String mimeType) {
+    public InMemoryFile(String filename, byte[] data, String mimeType)
+    {
         this.filename = filename;
         this.data = data;
         this.mimeType = mimeType;
     }
-    public InMemoryFile(String filename, InputStream stream, String mimeType) {
-        try {
+
+    public InMemoryFile(String filename, InputStream stream, String mimeType)
+    {
+        try
+        {
             data = ByteStreams.toByteArray(stream);
             this.filename = filename;
             this.mimeType = mimeType;
-        } catch(Exception e) {
+        }
+        catch (Exception e)
+        {
             data = new byte[0];
             filename = "Error";
         }
     }
-    public ExternalFile saveToStorage(Context context, Date date) {
-        if(ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+
+    public ExternalFile saveToStorage(Context context, Date date)
+    {
+        if (ContextCompat.checkSelfPermission(context,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions((Activity) context,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
             return null;
-        } else {
-            File pathToDir = new File(context.getExternalFilesDir(null), String.valueOf(date.getTime()/1000));
+        }
+        else
+        {
+            File pathToDir = new File(context.getExternalFilesDir(null),
+                    String.valueOf(date.getTime() / 1000));
             pathToDir.mkdir();
             File path = new File(pathToDir, filename);
 
-            try {
-                if(!path.exists()) {
+            try
+            {
+                if (!path.exists())
+                {
                     path.createNewFile();
                 }
                 FileOutputStream stream = new FileOutputStream(path);
                 stream.write(data);
                 stream.close();
                 return new ExternalFile(path, mimeType);
-            } catch(Exception exception) {
+            }
+            catch (Exception exception)
+            {
                 return null;
             }
         }
     }
-    public String getMimeType() {
+
+    public String getMimeType()
+    {
         return mimeType;
     }
-    public int getSize() {
+
+    public int getSize()
+    {
         return data.length;
     }
-    public boolean equals(InMemoryFile other) {
+
+    public boolean equals(InMemoryFile other)
+    {
         return Arrays.equals(data, other.data) &&
                 filename.equals(other.filename);
     }
