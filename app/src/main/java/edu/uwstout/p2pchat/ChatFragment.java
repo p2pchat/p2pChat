29e42abf4790e21448eca21465f99e369e572f37
Nicholas LaBelle <nrlabelle@gmail.com>
2019-11-08 10:54:40 -0600

    remove pointless stuff and fixed expresso tests

--- app/src/main/java/edu/uwstout/p2pchat/ChatFragment.java
+++ app/src/main/java/edu/uwstout/p2pchat/ChatFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -68,7 +69,7 @@ public class ChatFragment extends Fragment
             @Override
             public void onClick(View view)
             {
-
+                Toast.makeText(getContext(),"attachment clicked",Toast.LENGTH_SHORT);
             }
         });
 
