96cee05c385c8c5b70436072fbf901811f7bb961
Evan Vander Hoeven <vhevan.927@gmail.com>
2019-11-13 15:28:44 -0600

    Updated unit testing documentation to explain limitations
    Yeah, unit testing my functions are impossible. They will have to be instrumented tests, which are not fun but necessary.

--- app/src/test/java/edu/uwstout/p2pchat/WifiDirectTest.java
+++ app/src/test/java/edu/uwstout/p2pchat/WifiDirectTest.java
@@ -2,15 +2,18 @@ package edu.uwstout.p2pchat;
 
 
 import android.net.wifi.p2p.WifiP2pDevice;
-import android.net.wifi.p2p.WifiP2pDeviceList;
-import android.net.wifi.p2p.WifiP2pManager;
 
 import org.junit.Test;
 
 import static com.google.common.truth.Truth.assertThat;
 
 /**
- * Unit test class for the WifiDirect Singleton
+ * Unit test class for the WifiDirect Singleton.
+ * Note that only the static functions that don't
+ * require context in the WifiDirect singleton
+ * can be unit tested as all other functions
+ * need a context, something that is impossible
+ * within a unit test.
  */
 public class WifiDirectTest
 {
