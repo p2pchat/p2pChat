d34a49294e2fd51e5c5d34ca12bc87b6cca891a3
Evan Vander Hoeven <vhevan.927@gmail.com>
2019-11-17 11:24:47 -0600

    refactored tests to be more specific
    The tests will now assert errors in different spots depending on what went wrong, meaning that the tests will inform you where the error is. So far it is in serialization. It may also be in deserialization, but I won't know until I fix serialziation.

--- app/src/test/java/edu/uwstout/p2pchat/SerializationTest.java
+++ app/src/test/java/edu/uwstout/p2pchat/SerializationTest.java
@@ -29,46 +29,79 @@ public class SerializationTest
     {
         // Create the InMemoryFile we want to serialize
         InMemoryFile imf = new InMemoryFile("Hello World!");
+        InputStream inputStream = null;
+        // If the Bad Message isn't overwritten by deserialization, we assert false.
+        InMemoryFile imfRetrieved = new InMemoryFile("Bad Message!");
         try
         {
             // serialize it.
-            InputStream inputStream = SendDataService.serializeInMemoryFile(imf);
+            inputStream = SendDataService.serializeInMemoryFile(imf);
             assertThat(inputStream).isNotNull();
-            // deserialize it.
-            InMemoryFile imfRetrieved = ReceiverAsyncTask.parseInMemoryFileFromInputStream(inputStream);
-            assertThat(imfRetrieved).isNotNull();
-            // Check that the contents were preserved.
-            assertThat(imfRetrieved.getMimeType()).isEqualTo(imf.getMimeType());
-            assertThat(imfRetrieved.getTextMessage()).isEqualTo(imf.getTextMessage());
         }
         catch (Exception e)
         {
             assert(false); // exceptions are bad and should feel bad.
         }
+        try
+        {
+            // deserialize it.
+            imfRetrieved = ReceiverAsyncTask.parseInMemoryFileFromInputStream(inputStream);
+            assertThat(imfRetrieved).isNotNull();
+        }
+        catch (Exception e)
+        {
+            assert(false); // separate try/catch tells us where the error is occuring.
+        }
+        // Check that the contents were preserved.
+        assertThat(imfRetrieved.getMimeType()).isEqualTo(imf.getMimeType());
+        assertThat(imfRetrieved.getTextMessage()).isEqualTo(imf.getTextMessage());
     }
 
+    /**
+     * Checks that we can serialize and deserialize an
+     * InetAddress without data loss.
+     */
     @Test
     public void INetAddressSerialization()
     {
+        // Create the INetAddress we want to serialize.
+        byte[] ipAddr = new byte[]{127, 0, 0, 1};
+        InetAddress address = null;
+        InputStream inputStream = null;
+        InetAddress receivedAddress = null;
         try
         {
-            // Create the INetAddress we want to serialize.
-            byte[] ipAddr = new byte[]{127, 0, 0, 1};
-            InetAddress address = InetAddress.getByAddress(ipAddr);
+            address = InetAddress.getByAddress(ipAddr);
+        }
+        catch (UnknownHostException e)
+        {
+            // This isn't a good error, because the test fails
+            // before we get to our own code.
+            assert(false);
+        }
+        try
+        {
             // serialize it.
-            InputStream inputStream = SendDataService.serializeINetAddress(address);
+            inputStream = SendDataService.serializeINetAddress(address);
             assertThat(inputStream).isNotNull();
-            // deserialize it.
-            InetAddress receivedAddress =
-                    UpdaterAsyncTask.parseInetAddressFromInputStream(inputStream);
-            assertThat(receivedAddress).isNotNull();
-            // Check that the contents were preserved.
-            assertThat(receivedAddress.getCanonicalHostName())
-                    .isEqualTo(address.getCanonicalHostName());
         }
         catch (Exception e)
         {
             assert(false); // exceptions are inherently bad in testing
         }
+
+        try
+        {
+            // deserialize it.
+            receivedAddress = UpdaterAsyncTask.parseInetAddressFromInputStream(inputStream);
+            assertThat(receivedAddress).isNotNull();
+        }
+        catch (Exception e)
+        {
+            assert(false);
+        }
+        // Check that the contents were preserved.
+        assertThat(receivedAddress.getCanonicalHostName())
+                .isEqualTo(address.getCanonicalHostName());
     }
 }
